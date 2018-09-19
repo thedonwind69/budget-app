3e2c955ae4a1d86e14e31e425d790cc3853f5141
Don Nguyen <thedonwind69@gmail.com>
2018-09-18 18:59:11 -0700

    add

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -29,6 +29,7 @@ class Greeting extends React.Component {
                     <h1>Budget App</h1>
                     <p>Manage your budget... on a budget (It's free)</p>
                 </div>
+            <HighlightsContainer />
             </div>
         )
     }
