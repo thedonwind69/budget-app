c8154796b80cb493193dcbc1d64194342c99abaf
Don Nguyen <thedonwind69@gmail.com>
2018-09-23 15:23:26 -0700

    add

--- frontend/components/home_page.jsx
+++ frontend/components/home_page.jsx
@@ -28,7 +28,6 @@ class HomePage extends React.Component {
                     <Link class={'nav-link nav-link-hover'} to="/signup">SignUp</Link>
                     <h1>if you are already logged in:</h1>
                     
-                    
                 </div>
        
         )
