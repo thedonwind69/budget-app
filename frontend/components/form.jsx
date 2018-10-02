fbf379169c4a4e4108a8ab8666684618a8d62e8c
Don Nguyen <thedonwind69@gmail.com>
2018-10-01 20:10:33 -0700

    add

---
+++ frontend/components/form.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+  } from 'react-router-dom';
+
+class Form extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+    
+    render () {
+        return (
+          
+                <div class="home-page-container">
+                    <h1>this is the form</h1>
+                </div>
+       
+        )
+    }
+
+}
+
+export default Form;
