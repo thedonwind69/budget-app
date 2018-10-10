f8f8a49aa77a9f4e68789a0dc63b68ef1e9aa2ba
Don Nguyen <thedonwind69@gmail.com>
2018-10-09 21:08:27 -0700

    add

---
+++ frontend/util/budget_api_util.js
@@ -0,0 +1,29 @@
+export const fetchBudgets = (user_id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/user/${city_id}/posts`
+    })
+};
+
+export const createPost = (post) => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/posts',
+        data: {post}
+    })
+);
+
+export const deletePost = (user_id, post_id) => (
+    $.ajax({
+        method: 'DELETE',
+        url: `api/users/${user_id}/posts/${post_id}`
+    })
+)
+
+export const updatePost = (post, user_id, post_id) => (
+    $.ajax({
+        method: 'PUT',
+        url: `api/users/${user_id}/posts/${post_id}`,
+        data: {post}
+    })
+)
