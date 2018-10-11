842d7cc429aa45ece690b5205fc52a8cd0fb0f8a
Don Nguyen <thedonwind69@gmail.com>
2018-10-10 18:17:52 -0700

    add

--- frontend/components/new_budget_form_container.js
+++ frontend/components/new_budget_form_container.js
@@ -1,12 +1,13 @@
 import {connect} from 'react-redux';
 import NewBudgetForm from './new_budget_form';
 
+
 const mapStateToProps = (state) => ({
     currentUser: state.session.currentUser
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    
+
 })
 
 const NewBudgetFormContainer = connect(mapStateToProps, mapDispatchToProps)(NewBudgetForm);
