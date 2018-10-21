04c44a28ff17ad520bc3317dd0156d766162c081
Don Nguyen <thedonwind69@gmail.com>
2018-10-20 18:57:05 -0700

    add

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -18,6 +18,7 @@ import {AuthRoute, ProtectedRoute} from '../util/route_util.jsx';
 import SessionFormContainer from './session_form_container';
 
 import YourBudgetsContainer from './your_budgets_container';
+import EditBudget from './edit_budget';
 
 class App extends React.Component {
 
@@ -33,6 +34,7 @@ class App extends React.Component {
                     <Route exact path="/" component={GreetingContainer} />
 
                     <Route path="/your_budgets" component={YourBudgetsContainer} />
+                    <Route path="/budgets/:budget_id" component={EditBudget} />
 
                     <AuthRoute path="/login" component={SessionFormContainer} />
                     <AuthRoute path="/signup" component={SessionFormContainer} />
