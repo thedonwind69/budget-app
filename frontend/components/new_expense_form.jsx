5ee348d3594ec2cd4d559e18be3db19ae1125a04
Don Nguyen <thedonwind69@gmail.com>
2018-11-05 20:45:04 -0800

    add

--- frontend/components/new_expense_form.jsx
+++ frontend/components/new_expense_form.jsx
@@ -87,7 +87,6 @@ class NewExpenseForm extends React.Component {
         this.setState({
             category: null,
             amount: null,
-            date: null,
             description: null
         })
     }
@@ -103,53 +102,73 @@ class NewExpenseForm extends React.Component {
 
     displayAllExpenses () {
         var {currentExpenses} = this.props;
+        var displayExpenses = currentExpenses.map((expense) => {
+            return (
+                <li>
+                    <div>
+                        <h1>{expense.date} : {expense.category}</h1>
+                        <p>${expense.amount}</p>
+                        <p>{expense.description}</p>
+                    </div>
+                </li>
+            )
+        })
+        return displayExpenses.reverse();
 
     }
 
     render () {
-        var {currentBudget, takeHomePayDataset, currentExpenses} = this.props;
+        var {takeHomePayDataset} = this.props;
+     
         return (
             <div>
                 <h1>Your take-home monthly pay is : {takeHomePayDataset[0].amount}</h1>
                 <h1>Your take-home monthly pay AFTER expenses is :{this.afterExpenses()}</h1>
-                <div ref='pieChart' class='left' id='pie-chart'>
-
-                </div>
 
                 <div>
+                    <div ref='pieChart' class='left' id='pie-chart'>
 
+                    </div>
+
+                    <button ref='pieButton' onClick={this.generatePayInfo.bind(this)} class='post-submit-button'>View Paycheck Info:</button>
+
+                    <div ref='postForm' class={`post-form-container`}>
+                        <form ref="postFormReset" onSubmit={this.submitExpense.bind(this)}>
+                    
+                                <br />
+                                <select class='post-category-dropdown' ref='category' id='category' onChange={this.update('category')}>
+                                    <option value="" selected disabled hidden>Choose Category</option>
+                                    <option value="Food">Food</option>
+                                    <option value="Entertainment">Entertainment</option>
+                                    <option value="Utilities">Utilities</option>
+                                    <option value="Misc">Misc</option>
+                                </select>
+                                <br />
+                            <label for='amount'>Amount</label>
+                                <br/>
+                            <input class='post-subject' id='amount' type='number' onChange={this.update('amount')}/>
+                                <br />
+                            <label for='date'>Date</label>
+                                <br/>
+                            <input class='post-subject' id='date' type='date' onChange={this.update('date')}/>
+                                <br/>
+                            <label for='description'>Description</label>
+                                <br/>
+                            <input class='post-subject' id='description' type='text' onChange={this.update('description')}/>
+                                <br/>
+                            <input class='post-submit-button' type='submit' value='Add New Expense' />
+                        </form>
+                    </div>
                 </div>
-                
-                <button ref='pieButton' onClick={this.generatePayInfo.bind(this)} class='post-submit-button'>View Paycheck Info:</button>
 
-                 <div ref='postForm' class={`right post-form-container`}>
-                    <form ref="postFormReset" onSubmit={this.submitExpense.bind(this)}>
-                    <label for='category'>Category</label>
-                            <br />
-                            <select class='post-category-dropdown' ref='category' id='category' onChange={this.update('category')}>
-                                <option value="" selected disabled hidden>Choose Category</option>
-                                <option value="Food">Food</option>
-                                <option value="Entertainment">Entertainment</option>
-                                <option value="Utilities">Utilities</option>
-                                <option value="Misc">Misc</option>
-                            </select>
-                            <br />
-                        <label for='amount'>Amount</label>
-                            <br/>
-                        <input class='post-subject' id='amount' type='number' onChange={this.update('amount')}/>
-                            <br />
-                        <label for='date'>Date</label>
-                            <br/>
-                        <input class='post-subject' id='date' type='text' onChange={this.update('date')}/>
-                            <br/>
-                        <label for='description'>Description</label>
-                            <br/>
-                        <input class='post-subject' id='description' type='text' onChange={this.update('description')}/>
-                            <br/>
-                        <input class='post-submit-button' type='submit' value='Add New Expense' />
-                    </form>
+
+                <div class='clearfix'></div>
+
+                <div class='all-expenses'>
+                    <ul>
+                        {this.displayAllExpenses()}
+                    </ul>
                 </div>
-
 
             </div>
         )
