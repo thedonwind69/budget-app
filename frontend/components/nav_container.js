baff8eddc5d0ae82530b192a066b74eaf6bdc199
Don Nguyen <thedonwind69@gmail.com>
2018-11-28 19:33:48 -0800

    yes

--- frontend/components/nav_container.js
+++ frontend/components/nav_container.js
@@ -1,16 +1,12 @@
 import {connect} from 'react-redux';
-
 import {logout} from '../actions/session_actions';
-
 import Nav from './nav';
 
 const mapStateToProps = (state) => ({
     currentUser: state.session.currentUser
 });
 
-
 const mapDispatchToProps = (dispatch) => ({
-
     logout: () => dispatch(logout())
 });
 
