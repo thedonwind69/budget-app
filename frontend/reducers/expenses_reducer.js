94405dbb8350489f05d102248fb83a69057e2c5d
Don Nguyen <thedonwind69@gmail.com>
2018-12-27 09:49:32 -0800

    add

--- frontend/reducers/expenses_reducer.js
+++ frontend/reducers/expenses_reducer.js
@@ -5,7 +5,6 @@ const expensesReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
         case RECEIVE_EXPENSES:
-            // return action.expenses;
             const newState = {};
             action.expenses.forEach((expense) => {
                 newState[expense.id] = expense;
